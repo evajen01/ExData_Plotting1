87a2a5efa9b010a858e1f942d198f253819d571d
EV010409 <eric.vajentic@cerner.com>
2015-03-08 17:56:31 -0500

    Plot 3 and code

---
+++ plot3.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/EV010409/Documents/Coursera/Exploratory_Data_Analysis")
+data<-read.table("household_power_consumption.txt", sep=";", header=TRUE)
+data$Global_active_power<-as.numeric(levels(data$Global_active_power))[data$Global_active_power]
+data$DateFilter<-as.Date(as.character(data$Date), '%d/%m/%Y')
+date$TimeFilter<-as.character(data$Time)
+subdata<-subset(data, DateFilter >= '2007-02-01' & DateFilter <= '2007-02-02')
+subdata$DateWhole<-paste(subdata$DateFilter, as.character(subdata$Time))
+subdata$DateP<-as.POSIXct(subdata$DateWhole)
+
+png(file="plot3.png")
+plot(subdata$DateP,subdata$Sub_metering_1, xlab="", ylab="Energy sub metering", col="black", type="l")
+lines(subdata$DateP,subdata$Sub_metering_2, col="red")
+lines(subdata$DateP,subdata$Sub_metering_3, col="blue")
+legend("topright", pch=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
